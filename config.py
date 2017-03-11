4ee4904daf8af5f7ba0e0a79df713ce608999ffb
Chung-Heng Yeh <chungheng.yeh@gmail.com>
2017-03-11 16:53:05 -0500

    include sandbox folder

--- config.py
+++ config.py
@@ -9,12 +9,12 @@ parser.add_argument("--filename", default="config.json", type=str, help="directo
 parser.add_argument("--path", default="components/.crossbar/", type=str, help="directory to place the generated configuration file")
 
 add_nlp = parser.add_argument_group('nlp', 'arguments for setting up NeuroNLP')
-add_nlp.add_argument("--nlp-path", dest='nlp_path', default="../web/ffbo.neuronlp", type=str, help="path to the NeuroNLP folder")
+add_nlp.add_argument("--nlp-path", dest='nlp_path', default="./components/web/ffbo.neuronlp", type=str, help="path to the NeuroNLP folder")
 add_nlp.add_argument("--nlp-port", dest='nlp_port', default=8081, type=int, help="port number for hosting NeuroNLP, default is 8081")
 
 
 add_gfx = parser.add_argument_group('gfx', 'arguments for setting up NeuroGFX')
-add_gfx.add_argument("--gfx-path", dest='gfx_path', default="../web/ffbo.neurogfx", type=str, help="path to the NeuroGFX folder")
+add_gfx.add_argument("--gfx-path", dest='gfx_path', default="./components/web/ffbo.neurogfx", type=str, help="path to the NeuroGFX folder")
 add_gfx.add_argument("--gfx-port", dest='gfx_port', default=8082, type=int, help="port number for hosting NeuroGFX, default is 8082")
 
 add_ssl = parser.add_argument_group('ssl', 'arguments for setting up ssl connection')
@@ -23,6 +23,12 @@ add_ssl.add_argument("--ssl-cert", dest='ssl_cert', default="", help="path to th
 add_ssl.add_argument("--ssl-key", dest='ssl_key', default="", help="path to the key file")
 parser.set_defaults(ssl=False)
 
+add_sandbox = parser.add_argument_group('sandbox', 'arguments for setting up sandbox')
+add_sandbox.add_argument('--no-sandbox', dest='sand_box', action='store_false', help='disable sandbox directory; sandbox is enabled by default')
+add_sandbox.add_argument("--sandbox-path", dest='sandbox_path', default="./components/web/sandbox", type=str, help="path to the sandbox folder")
+add_sandbox.add_argument("--sandbox-port", dest='sandbox_port', default=8083, type=int, help="port number for hosting sandbox, default is 8083")
+parser.set_defaults(sandbox=True)
+
 args = parser.parse_args()
 
 print("Generating Crossbar configuration file...")
@@ -30,13 +36,20 @@ print("Generating Crossbar configuration file...")
 # load default configuration
 default_config = json.load(open("components/.crossbar/default_config.json"));
 
+# handle sandbox options
+if args.sandbox:
+    default_config["workers"][0]["transports"][2]["endpoint"]["port"] = args.sandbox_port
+    default_config["workers"][0]["transports"][2]["paths"]["/"]["directory"] = os.path.realpath(args.sandbox_path)
+else:
+    del default_config["workers"][0]["transports"][2]
+
 # handle SSL options
 if args.ssl:
-    for i in xrange(2):
+    for i in xrange(len(default_config["workers"][0]["transports"])):
         default_config["workers"][0]["transports"][i]["endpoint"]["tls"]["certificate"] = args.ssl_cert
         default_config["workers"][0]["transports"][i]["endpoint"]["tls"]["key"] = args.ssl_key
 else:
-    for i in xrange(2):
+    for i in xrange(len(default_config["workers"][0]["transports"])):
         del default_config["workers"][0]["transports"][i]["endpoint"]["tls"]
 
 # handle NLP options
