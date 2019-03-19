22f32bbde67be40850dfd12a449404303ae08de0
Yiyin Zhou <yiyin@ee.columbia.edu>
2019-03-19 16:46:06 -0400

    more fix

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ else:
         ip = res.text
     except Timeout:
         ip = "localhost"
-port = '{}{}'.format(1, int(config["ID"]["digits"])*10)
+port = int(config["ID"]["digits"])*10+10000
 processor_url = "%(ws)s://%(ip)s:%(port)s/ws" % {"ws":websockets, "ip":ip, "port":port}
 
 
