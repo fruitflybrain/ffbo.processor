d26c3774fafa7f399cd5dd3fc38669fa0ca24e50
Yiyin Zhou <yiyin@ee.columbia.edu>
2019-04-15 17:47:01 -0400

    update crawl.py using latest flycircuit syntax

--- components/processor_component/crawl.py
+++ components/processor_component/crawl.py
@@ -10,8 +10,8 @@ def clean(s):
 
 def extract(s):
     js = {}
-    js['Name'] = s.split('(')[0]
-    s = s.split('(')[1].rstrip(')')
+    js['Name'] = s.split('[')[0]
+    s = s.split('[')[1].rstrip(']')
     for si in s.split(','):
         k, v = si.split(':')
         js[clean(k)] = clean(v)
@@ -37,8 +37,8 @@ class FlyCircuitDB(object):
             return self.flycircuit_cache[neuron]
 
         url = 'http://flycircuit.tw/modules.php?name=clearpage&op=detail_table&neuron=' + neuron
-        req = requests.get(url, timeout = 2.0)
-        req.raise_for_status
+        req = requests.get(url, timeout = 8.0)
+        req.raise_for_status()
         soup = BeautifulSoup(req.text, 'html.parser')
         tds = soup.find_all('td')
 
@@ -56,27 +56,27 @@ class FlyCircuitDB(object):
 
         # Image
         js['Images'] = {}
-        for i in range(24, 24+3):
+        for i in range(22, 22+2):
             img_src = "http://flycircuit.tw" + tds[i+3].find('a', href=True).get('href')
             thread.start_new_thread(fetch_image, (img_src,))
             js['Images'][clean(tds[i].text)] = prefix_img_to_frontend + img_src.split('/')[-1]
 
         # Spatial Distribution
-        js['Spatial Distribution'] = [int(x) for x in tds[31].img.get('src').split('=')[-1].split('_')]
+        js['Spatial Distribution'] = [int(x) for x in tds[29].img.get('src').split('=')[-1].split('_')]
 
         # Similar neurons
         js['Similar neurons'] = []
-        for i in range(34, 34+4):
+        for i in range(38, 38+4):
             meta = extract(clean(tds[i].text))
             meta['img'] = tds[i+4].find('a', href=True).get('href')
             js['Similar neurons'].append(meta)
 
         # Cell body neighborhood
-        js['Cell body neighborhood'] = []
-        for i in range(44, 44+4):
-            meta = extract(clean(tds[i].text))
-            meta['img'] = tds[i+4].find('a', href=True).get('href')
-            js['Cell body neighborhood'].append(meta)
+        #js['Cell body neighborhood'] = []
+        #for i in range(44, 44+4):
+        #    meta = extract(clean(tds[i].text))
+        #    meta['img'] = tds[i+4].find('a', href=True).get('href')
+        #    js['Cell body neighborhood'].append(meta)
 
         self.flycircuit_cache[neuron] = js
         return js
